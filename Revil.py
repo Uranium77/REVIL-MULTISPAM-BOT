7329926e98a0425e373e2a4281414f1a6c7487c4
Uranium77 <98459212+Uranium77@users.noreply.github.com>
2022-02-07 15:41:13 +0530

    Update Revil.py

--- Revil.py
+++ Revil.py
@@ -1412,7 +1412,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"
+        await event.edit
 ██████╗░░█████╗░███╗░░██╗░██████╗░
 ██╔══██╗██╔══██╗████╗░██║██╔════╝░
 ██████╔╝██║░░██║██╔██╗██║██║░░██╗░
